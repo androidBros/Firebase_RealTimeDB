599730f5e1f55dcfdba3d007388d5592dfdb76d5
theo-no <tjsgh1512@naver.com>
2022-10-04 16:55:08 +0900

    firebase db inout check

---
+++ app/src/main/java/com/example/firebase_realtimedb/Camp.java
@@ -0,0 +1,14 @@
+package com.example.firebase_realtimedb;
+
+public class Camp {
+    public String campname;
+    public String location;
+
+    public Camp(){
+
+    }
+    public Camp(String campname, String location ){
+        this.campname = campname;
+        this.location = location;
+    }
+}
--- app/src/main/java/com/example/firebase_realtimedb/MainActivity.java
+++ app/src/main/java/com/example/firebase_realtimedb/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.firebase_realtimedb;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -10,11 +12,27 @@ import com.google.firebase.database.FirebaseDatabase;
 public class MainActivity extends AppCompatActivity {
 
     FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference myRef = database.getReference("message");
+    DatabaseReference myRef = database.getInstance().getReference();
+    Button add_btn;
+    int i = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        myRef.setValue("Okay");
+        add_btn = findViewById(R.id.add_btn);
+
+        add_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                writeCampInfo("Camp"+i,i+"번 째 캠핑장",i+"번 째 도시");
+                i++;
+            }
+        });
+    }
+
+    public void writeCampInfo(String campID, String campname, String location){
+        Camp camp = new Camp(campname, location);
+        myRef.child("Camps").child(campID).setValue(camp);
+
     }
 }
